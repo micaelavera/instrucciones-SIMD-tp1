66e6f3b3964486e76b53c0f7a925899e7ea0c6b9
Micaela <micavera2010@hotmail.com>
2019-11-11 15:52:46 -0300

    new changes

--- enmascarar_image.c
+++ enmascarar_image.c
@@ -24,7 +24,7 @@ void guardarArchivo(char *nombreArchivo,){
 // Main
 int main(int argc, char *argv[]){
 
-    if (argc != 3){
+    if (argc != 4){
         printf("Error al ingresar los parametros\n");
         return 1;
     }
@@ -32,19 +32,24 @@ int main(int argc, char *argv[]){
     char *imagen1 = argv[1];
     char *imagen2 = argv[2];
     char *mask = argv[3];
-    int ancho = 
-    int alto =
-    int cant = ancho * alto
+    //int ancho = 
+    //int alto =
+    //int cant = ancho * alto
 
-   fseek
-
-    cargarArchivo(imagen1);
-    cargarArchivo(imagen2);
-    cargarArchivo(mask);
-
-
-   
     //FILE *fpA = fopen(archivo_a,"r+");
     //FILE *fpB = fopen(archivo_b,"r+");
     //FILE *mask = fopen(mask,"r+");
+    cargarArchivo(imagen1); //cambiar 
+    cargarArchivo(imagen2);
+    cargarArchivo(mask);
+
+    //VER
+
+    //fread Lee datos desde fichero.
+    //fread(void *datos, size_t tamaño, size_t cantidad, FILE *fichero);
+   
+    
+    //enmascarar_c(a,b,mask,cant);
+
+
 }
