c4b853dad699121704a63de54fe56c738d1cc053
Micaela <micavera2010@hotmail.com>
2019-11-11 14:51:19 -0300

    commit inicial

---
+++ enmascarar_image.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+void enmascarar_c(unsigned char *a, unsigned char *b, unsigned char *mask, int cant);
+void enmascarar_asm(unsigned char *a, unsigned char *b, unsigned char *mask, int cant);
+
+
+void enmascarar_c(unsigned char *a, unsigned char *b, unsigned char *mask, int cant){
+}
+
+
+void cargarArchivo(char *nombreArchivo){
+    FILE *fp;
+    fp = fopen(nombreArchivo,"r+");
+    
+    fclose(fp);
+
+}
+
+void guardarArchivo(char *nombreArchivo,){
+
+}
+
+// Main
+int main(int argc, char *argv[]){
+
+    if (argc != 4){
+        printf("Error al ingresar los parametros\n");
+        return 1;
+    }
+
+    char *imagen1 = argv[1];
+    char *imagen2 = argv[2];
+    char *mask = argv[3];
+    int ancho = 
+    int alto =
+    int cant = ancho * alto
+
+   
+
+    cargarArchivo(imagen1);
+    cargarArchivo(imagen2);
+    cargarArchivo(mask);
+
+
+   
+    //FILE *fpA = fopen(archivo_a,"r+");
+    //FILE *fpB = fopen(archivo_b,"r+");
+    //FILE *mask = fopen(mask,"r+");
+}
